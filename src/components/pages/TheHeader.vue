066626e6c72ea3ebe60903f531339e239497b4f2
Vladislav <popkin003@list.ru>
2023-11-18 23:54:44 +0300

    exam_Task3: Draft version of header

--- src/components/pages/TheHeader.vue
+++ src/components/pages/TheHeader.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="header__container">
-    <div class="header__links" @click="toggleContent" v-show="isSmallScreen">
-      <ion-button fill="clear" color="dark">
-        <ion-icon slot="icon-only" :icon="menu"></ion-icon>
+    <div class="header__links" v-show="isSmallScreen">
+      <ion-button @click.native="toggleContent" fill="clear" color="dark">
+        <ion-icon :icon="menu"></ion-icon>
       </ion-button>
     </div>
+
     <div class="header__links" v-show="!isSmallScreen">
       <a href="#">New</a>
       <a href="#">Brands</a>
@@ -12,16 +13,31 @@
     </div>
 
     <router-link to="/" class="header__home-link">Home</router-link>
+
     <div class="header__icons">
       <ion-button fill="clear" color="dark" v-show="!isSmallScreen">
-        <ion-icon slot="icon-only" :icon="search"></ion-icon>
+        <ion-icon :icon="search"></ion-icon>
       </ion-button>
       <ion-button fill="clear" color="dark" v-show="!isSmallScreen">
-        <ion-icon slot="icon-only" :icon="person"></ion-icon>
+        <ion-icon :icon="person"></ion-icon>
       </ion-button>
       <ion-button fill="clear" color="dark">
-        <ion-icon slot="icon-only" :icon="basket"></ion-icon>
+        <ion-icon :icon="basket"></ion-icon>
       </ion-button>
+    </div>
+
+    <div v-if="showContent" class="menu__content">
+      <div class="menu__close">
+        <ion-button @click.native="toggleContent" fill="clear" color="dark">
+          <ion-icon :icon="closeIcon"></ion-icon>
+        </ion-button>
+      </div>
+      <div class="menu__links">
+        <router-link to="/" class="header__home-link">Home</router-link>
+        <a href="#">New</a>
+        <a href="#">Brands</a>
+        <a href="#">Apparel</a>
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +45,11 @@
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { IonIcon, IonButton } from "@ionic/vue";
-import { search, basket, menu, person } from "ionicons/icons";
+import { search, basket, menu, person, close } from "ionicons/icons";
 
 const showContent = ref(false);
 const isSmallScreen = ref(window.innerWidth <= 767);
+const closeIcon = close;
 
 const toggleContent = () => {
   showContent.value = !showContent.value;
